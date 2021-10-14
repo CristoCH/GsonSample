dcfadd9e507f72dcc721139f3e6ae77081ed047f
CristoCH <80289370+CristoCH@users.noreply.github.com>
2021-10-14 13:34:36 +0100

    main_fuera

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -12,21 +12,4 @@ public class Persona {
         this.apellido = apellido;
         this.edad = edad;
     }
-
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Nombre: ");
-        String n = entrada.nextLine();
-        System.out.println("Apellido: ");
-        String a = entrada.nextLine();
-        System.out.println("Edad");
-        int e = entrada.nextInt();
-
-        Persona p = new Persona(n,a,e);
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String json = gson.toJson(p);
-        System.out.println(json);
-
-    }
 }
